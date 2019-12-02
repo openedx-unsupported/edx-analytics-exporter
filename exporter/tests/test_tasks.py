ac9e014743593de835facc0a9c4f8504361bf643
Stu Young <estute@users.noreply.github.com>
2019-12-02 15:02:47 -0500

    handle course keys with non ascii characters (#41)
    handle course keys with non ascii characters

--- exporter/tests/test_tasks.py
+++ exporter/tests/test_tasks.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*
+
 from copy import deepcopy
 
 from exporter import tasks
@@ -22,7 +24,7 @@ def test_org_email_opt_in_task(mock_execute_shell):
     kwargs = {
         'organization': 'the-org',
         'other_names': ['the-second-org', 'the-third-org'],
-        'courses': ['course-1', 'course-2'],
+        'courses': ['course-1', u'coursé-2'],
         'django_config': 'the-django-config',
         'django_admin': 'the-django-admin',
         'django_settings': 'the-django-setings',
@@ -33,11 +35,11 @@ def test_org_email_opt_in_task(mock_execute_shell):
 
     command = tasks.OrgEmailOptInTask.run(filename, dry_run, **kwargs)
 
-    assert command.endswith('the-filename the-org the-second-org the-third-org --courses=course-1,course-2 --email-optin-chunk-size=10000')
+    assert command.endswith(u'the-filename the-org the-second-org the-third-org --courses=course-1,coursé-2 --email-optin-chunk-size=10000')
     assert 'email_opt_in_list' in command
     expected_kwargs = deepcopy(kwargs)
     expected_kwargs['all_organizations'] = 'the-org the-second-org the-third-org'
-    expected_kwargs['comma_sep_courses'] = 'course-1,course-2'
+    expected_kwargs['comma_sep_courses'] = u'course-1,coursé-2'
     expected_kwargs['max_tries'] = 3
     mock_execute_shell.assert_called_once_with(command, **expected_kwargs)
 
@@ -58,3 +60,12 @@ def test_get_filename_course_task():
         'course': 'course-v1:edX+DemoX+Demo_Course',
     }
     assert '/tmp/workdir/edX-DemoX-Demo_Course-test_course_task-test-analytics.csv' == TestCourseTask.get_filename(**kwargs)
+
+
+def test_get_non_ascii_filename_course_task():
+    kwargs = {
+        'name': 'test-analytics',
+        'work_dir': '/tmp/workdir/',
+        'course': u'course-v1:edX+DemoX+Démo_Course',
+    }
+    assert '/tmp/workdir/edX-DemoX-D_mo_Course-test_course_task-test-analytics.csv' == TestCourseTask.get_filename(**kwargs)
