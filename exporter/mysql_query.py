87ad53dbb5f77e5fc2270739fb072e0ca339663d
Hassan <hassan@edx.org>
2023-07-13 21:45:11 +0500

    fix: replaced null character(\x00) to fix escaping error while writin… (#87)
    * fix: replaced null character(\x00) to fix escaping error while writing to csv.
    
    * chore: Added test.
    
    ---------
    
    Co-authored-by: Hassan Javeed <hjaveed@2u.com>

--- exporter/mysql_query.py
+++ exporter/mysql_query.py
@@ -59,4 +59,4 @@ class MysqlDumpQueryToTSV(object):
 
     def _normalize_value(self, value):
         if value is None: value='NULL'
-        return str(value).replace('\\', '\\\\').replace('\r', '\\r').replace('\t','\\t').replace('\n', '\\n')
+        return str(value).replace('\\', '\\\\').replace('\r', '\\r').replace('\t','\\t').replace('\n', '\\n').replace('\x00', '')
